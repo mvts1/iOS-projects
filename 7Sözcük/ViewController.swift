7db1e0afcb9ff5cdca2705d8659f87776d3b04b5
mvts1 <omavitas@gmail.com>
2021-01-22 15:14:32 +0300

    Initial Commit

---
+++ 7Sözcük/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7Sözcük
+//
+//  Created by Onur Mavitas on 22.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
